fe46c2d367b0bd0bed5537900346cb837a8b637c
amorim <gabrielmaia.amorim01@gmail.com>
2023-04-10 01:00:50 -0300

    fix: category return validation

--- server/src/controllers/category.ts
+++ server/src/controllers/category.ts
@@ -5,6 +5,11 @@ import { CategoryDocument } from "../@types/models/ICategory";
 export const getAllCategories = async (req: Request, res: Response) => {
   try {
     const category: CategoryDocument[] = await Category.find();
+
+    if (category.length < 1) {
+      return res.status(404).send({ errorMessage: "No products found" });
+    }
+
     return res.status(200).send(category);
   } catch (err: any) {
     return res.status(400).send({ errorMessage: `Failed to get categories, ${err.message}` });
